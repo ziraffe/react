fb576f7173474d90abb59c921b5d22b8c186a92f
ziraffe <ziraffe0@gmail.com>
2021-09-16 11:45:54 +0900

    react

---
+++ src/App.js
@@ -0,0 +1,17 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="gray-back">
+      <header className="App-header">
+        <img src={logo} className="gray-back" alt="logo" />
+        <p>
+         aaaa
+        </p>
+      </header>
+    </div>
+  );
+}
+
+export default App;
